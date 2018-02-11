9b279c1f22245fe0061cd5e7599a12f68895d381
ravi4all <ravikanttyagi.2295@gmail.com>
2018-02-11 19:04:11 +0530

    Add files via upload

---
+++ PythonIntro/02-PythonInput.py
@@ -0,0 +1,11 @@
+#num_1 = int(input("Enter first number : "))
+#num_2 = int(input("Enter second number : "))
+
+num_1 = input("Enter first number : ")
+num_2 = input("Enter second number : ")
+
+#num_1 = int(num_1)
+
+result = int(num_1) + int(num_2)
+
+print("Result is",result)
